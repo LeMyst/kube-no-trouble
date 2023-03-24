6480f1578b7dfe7504102ec1fdb8b6fab1c6020f
Myst <1592048+LeMyst@users.noreply.github.com>
2023-03-24 10:03:44 +0100

    Fix test

--- test/rules_126_test.go
+++ test/rules_126_test.go
@@ -6,7 +6,6 @@ import (
 
 func TestRego126(t *testing.T) {
 	testCases := []resourceFixtureTestCase{
-		{"AutoScaler-v2beta1", []string{"../fixtures/autoscaler-v2beta1.yaml"}, []string{"HorizontalPodAutoscaler"}},
 		{"AutoScaler-v2beta2", []string{"../fixtures/autoscaler-v2beta2.yaml"}, []string{"HorizontalPodAutoscaler"}},
 	}
 
